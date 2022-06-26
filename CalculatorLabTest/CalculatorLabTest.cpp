5a2f821eff596282773c5c4c1632cef9631b4401
JohnWok <jtwokurka@student.fullsail.edu>
2022-06-26 18:46:46 -0400

    unit test

---
+++ CalculatorLabTest/CalculatorLabTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalculatorLabTest
+{
+	TEST_CLASS(CalculatorLabTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
