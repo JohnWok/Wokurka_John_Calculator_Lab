f27a1063a2c8675f47505ed373254d850d840887
JohnWok <jtwokurka@student.fullsail.edu>
2022-06-26 18:57:54 -0400

    unit test not working

--- CalculatorLabTest/CalculatorLabTest.cpp
+++ CalculatorLabTest/CalculatorLabTest.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../Wokurka_John_Calculator_Lab/ButtonFactory.h"
+#include "../Wokurka_John_Calculator_Lab/CalculatorProcessor.h"
+#include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,8 +12,19 @@ namespace CalculatorLabTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(CalculatorProcessorTests)
 		{
+			
+
 		}
+		
+		
+		TEST_METHOD(ButtonFactoryTests)
+		{
+
+
+		}
+
+		
 	};
 }
